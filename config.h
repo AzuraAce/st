897c6517848960f6774c98c8e1b78a9f363a2ac1
AzuraAce <oliverjbmub@gmail.com>
2025-04-02 14:41:30 +0200

    changed font size

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 
-static char *font = "Hack Nerd Font Mono:pixelsize=27:antialias=true:autohint=true"; /* "Undefined Medium:pixelsize=29:antialias=true:autohint=true" */
+static char *font = "Hack Nerd Font Mono:pixelsize=30:antialias=true:autohint=true"; /* "Undefined Medium:pixelsize=29:antialias=true:autohint=true" */
 /* Spare fonts */
 static char *font2[] = {
 /*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
