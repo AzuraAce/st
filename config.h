3e332e20bad00d7508d6dce47c81398f2b49de15
AzuraAce <oliverjbmub@gmail.com>
2025-04-15 02:58:30 +0200

    made colors more vibrant

--- config.h
+++ config.h
@@ -118,23 +118,23 @@ float alpha = 0.8;
 static const char *colorname[] = {
 	/* 8 normal colors */
 	"#45475A",
-	"#F38BA8",
-	"#A6E3A1",
-	"#F9E2AF",
-	"#89B4FA",
-	"#F5C2E7",
-	"#94E2D5",
+	"#f33167",
+	"#40e333",
+	"#f9bb32",
+	"#4187fa",
+	"#f949c8",
+	"#46e2c8",
 	"#BAC2DE",
 
 	/* 8 bright colors */
 	"#585B70",
-	"#F38BA8",
-	"#A6E3A1",
-	"#F9E2AF",
-	"#89B4FA",
-	"#F5C2E7",
-	"#94E2D5",
-	"#A6ADC8",
+	"#f33167",
+	"#40e333",
+	"#f9bb32",
+	"#4187fa",
+	"#f949c8",
+	"#46e2c8",
+	"#FFFFFF",
 
 	[256] = "#CDD6F4", /* default foreground colour */
 	[257] = "#1E1E2E", /* default background colour */
