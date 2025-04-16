f927ab33e3d5e117a94efb346f135d549a19366a
AzuraAce <oliverjbmub@gmail.com>
2025-04-16 03:59:02 +0200

    changed zoom bindings to TERMOD + -/+

--- config.h
+++ config.h
@@ -9,7 +9,7 @@
 static char *font = "HackNerdFontMono-Regular:pixelsize=30:antialias=true:autohint=true"; /* "DepartureMonoNerdFontMono:pixelsize=30:antialias=true:autohint=true" */
 /* Spare fonts */
 static char *font2[] = {
-	"Hack Nerd Font Mono:pixelsize=30:antialias=true:autohint=true", 
+	"monospace:pixelsize=30:antialias=true:autohint=true", 
 };
 
 static int borderpx = 2;
@@ -214,8 +214,8 @@ static Shortcut shortcuts[] = {
 	{ ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
 	{ ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
 	{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
-	{ TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
-	{ TERMMOD,              XK_Next,        zoom,           {.f = -1} },
+	{ TERMMOD,              XK_asterisk,    zoom,           {.f = +1} },
+	{ TERMMOD,              XK_underscore,  zoom,           {.f = -1} },
 	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
 	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
